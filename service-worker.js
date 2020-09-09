392084fa401026d2c3c6232cac092efc53ccdf40
mayurpatild <mayurpatild@users.noreply.github.com>
2020-09-09 20:34:51 +0000

    Deploying to gh-pages from  @ c4d71ba8e7c9a3d620a348c8dbffc89fb491fd03 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.31c417e0b18354db6318044272bd2e75.js"
+  "/precache-manifest.ed0e225757ef9ab16a5668edc873f39d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
