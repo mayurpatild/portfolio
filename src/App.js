f29130fe678df1de1c6837f2ffc32dad9fff20a2
Mayur Patil <mayurpatild@users.noreply.github.com>
2020-09-11 22:01:49 +0530

    updated App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React, { Fragment } from "react";
 import { BrowserRouter, Route } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle.min";
 import "./App.css";
 import Main from "./containers/Main";
 
 function App() {
   return (
     <div>
-      <Main class="title jumbotron jumbotron-fluid bg-transparent bgstyle text-light min-vh-100 d-flex align-content-center align-items-center flex-wrap m-0"/>
+      <Main />
     </div>
   );
 }
