7b262d084e2c3878668b0980c397d9e81bddd89c
mayurpatild <mayurpatild@users.noreply.github.com>
2020-09-11 18:14:56 +0000

    Deploying to gh-pages from  @ 958ab63f81f22d06bba6def04059b34f6381036a 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.90deb4ef71c9affadd1fed03a2980f7d.js"
+  "/portfolio/precache-manifest.f9b8979fd3b1a80a809625b50f723ac0.js"
 );
 
 self.addEventListener('message', (event) => {
