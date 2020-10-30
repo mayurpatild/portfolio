01e38e4d115483263ea4408500b0dbe14d01c1e6
mayurpatild <mayurpatild@users.noreply.github.com>
2020-10-30 17:31:07 +0000

    Deploying to gh-pages from  @ 282e752075fe33808eb433c9aeb7a5db3da004e1 🚀

---
+++ precache-manifest.91d3cb0b0fa8e7295a01932b30b23643.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e015a4eff6cb8722014eaf2673fa82fe",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "f1d8a736ba567e75b8a6",
+    "url": "/portfolio/static/css/3.f9898e9a.chunk.css"
+  },
+  {
+    "revision": "7f15311fd7cd87cade6e",
+    "url": "/portfolio/static/css/4.1c13673c.chunk.css"
+  },
+  {
+    "revision": "898ed2fda83b3b4dcd30",
+    "url": "/portfolio/static/css/main.4b7fe448.chunk.css"
+  },
+  {
+    "revision": "fef4241551d8993fc61e",
+    "url": "/portfolio/static/js/2.33bfc829.chunk.js"
+  },
+  {
+    "revision": "bf9deec973e9f328b23f7fff43b775a3",
+    "url": "/portfolio/static/js/2.33bfc829.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1d8a736ba567e75b8a6",
+    "url": "/portfolio/static/js/3.3dab85da.chunk.js"
+  },
+  {
+    "revision": "7f15311fd7cd87cade6e",
+    "url": "/portfolio/static/js/4.8e152ad9.chunk.js"
+  },
+  {
+    "revision": "898ed2fda83b3b4dcd30",
+    "url": "/portfolio/static/js/main.77a230e9.chunk.js"
+  },
+  {
+    "revision": "294e42691985a0a59e0b",
+    "url": "/portfolio/static/js/runtime-main.062d5f4c.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/portfolio/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "8aec0e4f489d9fba786d80a33a6849a0",
+    "url": "/portfolio/static/media/ISTQB.8aec0e4f.png"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/portfolio/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "a21738a4656b9900ec8e451ae3b7fbc8",
+    "url": "/portfolio/static/media/UiPath.a21738a4.webp"
+  },
+  {
+    "revision": "ca7e893295e71c3e0810b0e87cbaef57",
+    "url": "/portfolio/static/media/cbseLogo.ca7e8932.png"
+  },
+  {
+    "revision": "68e841388612be0c5143e2932fb77d4f",
+    "url": "/portfolio/static/media/contactMail.68e84138.webp"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/portfolio/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/portfolio/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "5947fcb1f570d687a211ea0bc3f8e2c5",
+    "url": "/portfolio/static/media/moon.5947fcb1.svg"
+  },
+  {
+    "revision": "9330604d1204651144c2fdda84691afc",
+    "url": "/portfolio/static/media/safe.9330604d.png"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/portfolio/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "5973653b4ad83fd0e8aa342fa7422099",
+    "url": "/portfolio/static/media/sppuPune.5973653b.jpg"
+  },
+  {
+    "revision": "2a5a3b478475adb24f3f8c8093a6267a",
+    "url": "/portfolio/static/media/sun.2a5a3b47.svg"
+  },
+  {
+    "revision": "245c1edd9020ccd06b425cfa0376a909",
+    "url": "/portfolio/static/media/tcs3.245c1edd.jfif"
+  },
+  {
+    "revision": "8c40fd512a31dc9c30110649c84ec5cd",
+    "url": "/portfolio/static/media/wuLogo.8c40fd51.png"
+  }
+]);
